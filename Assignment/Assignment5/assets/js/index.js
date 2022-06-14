4b56df973a3e5fe74b1a81063f6f351aa569b37f
Tej Bahadur Thapa <mgr9816112567@gmail.com>
2022-06-15 00:23:16 +0545

    to do list upload

---
+++ Assignment/Assignment5/assets/js/index.js
@@ -0,0 +1,19 @@
+document.querySelector('#add').onclick = function () {
+    if (document.querySelector('#tasks input').value.length == 0) {
+        alert("Please enter some task to be done!");
+    }
+    else {
+        document.querySelector('#added-tasks').innerHTML += `
+        <div class="added-task"> 
+        <span id="task-name">${document.querySelector('#tasks input').value} </span>
+        <button class = "delete"> Delete
+        </button>
+        </div>`;
+        var toDeleteTask = document.querySelectorAll(".delete");
+        for (var q = 0; q < toDeleteTask.length; q++) {
+            toDeleteTask[q].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
